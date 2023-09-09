e94c921b873919606d39dc47ee5607b8f44221ab
Ramil Khamitov <SurrealRam42@gmail.com>
2023-09-09 17:48:09 +0300

    Use junit test and update spring

--- src/test/java/ru/ramlabs/gitea/stonks/api/UserTest.java
+++ src/test/java/ru/ramlabs/gitea/stonks/api/UserTest.java
@@ -1,21 +1,22 @@
 package ru.ramlabs.gitea.stonks.api;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.testng.annotations.Test;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-public class AccountTest extends AbstractTestNGSpringContextTests {
+@ActiveProfiles("development")
+public class UserTest  {
 
     @Autowired
     public PasswordEncoder passwordEncoder;
 
     @Test
-    public void testGoodPasswordEncoding() {
+    public void testBadPasswordEncoding() {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < 100; i++) {
             builder.append("a");
@@ -23,6 +24,5 @@ public class AccountTest extends AbstractTestNGSpringContextTests {
         var encoded = passwordEncoder.encode(builder);
         assertThat(passwordEncoder.matches(builder.append("qqqq"), encoded)).isFalse();
     }
-
 
 }
